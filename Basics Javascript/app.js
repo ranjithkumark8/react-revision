ef34bb2b2756675d02d7ce969ea6827d860dfe64
Ranjith Kommuri <ranjith_kommuri@epam.com>
2023-12-06 15:21:45 +0530

    adding parcel to existing project

--- Basics Javascript/app.js
+++ Basics Javascript/app.js
@@ -1,30 +1,17 @@
-// /**
-//  *
-//  * <div id='parent'>
-//  *  <div id='child>
-//  *      <h1>H1 Tag</h1>
-//  *      <h2>H2 Tag</h2>
-//  * </div>
-//  *  <div id='child2>
-//  *      <h1>H1 Tag</h1>
-//  *      <h2>H2 Tag</h2>
-//  * </div>
-//  * </div>
-//  *
-//  */
-
-// const heading = React.createElement('div', { id: 'parent' }, [
-// 	React.createElement('div', { id: 'child' }, [
-// 		React.createElement('h1', {}, 'H1 Tag'),
-// 		React.createElement('h2', {}, 'H2 Tag'),
-// 	]),
-// 	React.createElement('div', { id: 'child2' }, [
-// 		React.createElement('h1', {}, 'H1 Tag'),
-// 		React.createElement('h2', {}, 'H2 Tag'),
-// 	]),
-// ]);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement('div', { id: 'parent' }, [
+	React.createElement('div', { id: 'child' }, [
+		React.createElement('h1', {}, 'H1 Tag'),
+		React.createElement('h2', {}, 'H2 Tag'),
+	]),
+	React.createElement('div', { id: 'child2' }, [
+		React.createElement('h1', {}, 'H1 Tag'),
+		React.createElement('h2', {}, 'H2 Tag'),
+	]),
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
 // let x = function (A) {
 // 	// Just write your code below to complete the function. Required input is available to you as the function arguments.
 // 	// Do not print the result or any output. Just return the result via this function.
@@ -46,4 +33,4 @@
 
 // x([74, 9, 51, 51, 75, 0, 35, 89, 96, 77]);
 
-console.log(Object.keys({ a: 1, B: 1 }).length);
+// console.log(Object.keys({ a: 1, B: 1 }).length);
